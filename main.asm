2e5de97b73b75897dc16caf7b0da4a3a947c4448
Spencer Blackwell <spencerblackwell02@gmail.com>
2023-04-04 17:39:14 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -20,7 +20,7 @@ sf80: .DB "DC=25.0%"	; create a static string in program memory
 buttonOn: .DB "Fan: On " //length 8
 buttonOff: .DB "Fan: Off" //length 8
 
-
+/*
 .DEF DREM16UL = R23
 .DEF DREM16UH = R24
 .DEF DRES16UL = R26
@@ -29,7 +29,7 @@ buttonOff: .DB "Fan: Off" //length 8
 .DEF DD16UH = R27
 .DEF DV16UL = R19
 .DEF DV16UH = R20
-
+*/
 
 start:
 	cli						; disable interrupts
@@ -435,3 +435,54 @@ compare:
 	cpi R22, 0b11100001
 	breq changeSpeedClock
 	rjmp poll2
+
+;***************************************************************************
+;*
+;* "div16u" - 16/16 Bit Unsigned Division
+;*
+;* This subroutine divides the two 16-bit numbers 
+;* "dd8uH:dd8uL" (dividend) and "dv16uH:dv16uL" (divisor). 
+;* The result is placed in "dres16uH:dres16uL" and the remainder in
+;* "drem16uH:drem16uL".
+;*  
+;* Number of words	:19
+;* Number of cycles	:235/251 (Min/Max)
+;* Low registers used	:2 (drem16uL,drem16uH)
+;* High registers used  :5 (dres16uL/dd16uL,dres16uH/dd16uH,dv16uL,dv16uH,
+;*			    dcnt16u)
+;*
+;***************************************************************************
+
+;***** Subroutine Register Variables
+
+
+.DEF DREM16UL = R23
+.DEF DREM16UH = R24
+.DEF DRES16UL = R26
+.DEF DRES16UH = R27
+.DEF DD16UL = R26
+.DEF DD16UH = R27
+.DEF DV16UL = R19
+.DEF DV16UH = R20
+.def dcnt16u = r21
+;***** Code
+
+div16u:	clr	drem16uL	;clear remainder Low byte
+	sub	drem16uH,drem16uH;clear remainder High byte and carry
+	ldi	dcnt16u,17	;init loop counter
+d16u_1:	rol	dd16uL		;shift left dividend
+	rol	dd16uH
+	dec	dcnt16u		;decrement counter
+	brne	d16u_2		;if done
+	ret			;    return
+d16u_2:	rol	drem16uL	;shift dividend into remainder
+	rol	drem16uH
+	sub	drem16uL,dv16uL	;remainder = remainder - divisor
+	sbc	drem16uH,dv16uH	;
+	brcc	d16u_3		;if result negative
+	add	drem16uL,dv16uL	;    restore remainder
+	adc	drem16uH,dv16uH
+	clc			;    clear carry to be shifted into result
+	rjmp	d16u_1		;else
+d16u_3:	sec			;    set carry to be shifted into result
+	rjmp	d16u_1
